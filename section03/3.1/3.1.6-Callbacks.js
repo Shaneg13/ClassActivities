5cc0f1f6899b905d79bb8ea271ce9dea9c297ec5
Shaneg13 <sgrewell713@gmail.com>
2022-11-14 17:48:42 -0500

    Updating ClassActivities

--- section03/3.1/3.1.6-Callbacks.js
+++ section03/3.1/3.1.6-Callbacks.js
@@ -20,29 +20,29 @@
   // pass each "regular" function into getAndShowMessage as a callback function
   getNameAndShowMessage(informAboutSale);
 
-//   // "regular" functions
-// function greet(name) {
-//   return `Hello ${name}!`;
-// }
+  // "regular" functions
+function greet(name) {
+  return `Hello ${name}!`;
+}
 
-// function welcome(name) {
-//   return `${name}, welcome!`;
-// }
+function welcome(name) {
+  return `${name}, welcome!`;
+}
 
-// function informAboutSale(name) {
-//   return `${name}, we're having a sale!`;
-// }
+function informAboutSale(name) {
+  return `${name}, we're having a sale!`;
+}
 
-// // function that takes a callback function as an argument
-// function getNameAndShowMessage(callback) {
-//   const name = prompt("Please Enter Your Name: ");
-//   alert(callback(name));
-// }
+// function that takes a callback function as an argument
+function getNameAndShowMessage(callback) {
+  const name = prompt("Please Enter Your Name: ");
+  alert(callback(name));
+}
 
-// Now pass each "regular" function into getNameAndShowMessage function as a callback function like so...
+//Now pass each "regular" function into getNameAndShowMessage function as a callback function like so...
 
-// console.log(getNameAndShowMessage(greet));
+console.log(getNameAndShowMessage(greet));
 
-// console.log(getNameAndShowMessage(welcome));
+console.log(getNameAndShowMessage(welcome));
 
-// console.log(getNameAndShowMessage(informAboutSale));
+console.log(getNameAndShowMessage(informAboutSale));
